237cf888952f25aaa191886cae9727b25be7956f
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-04-01 17:21:52 +0000

    added js code

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Aimar Fernandez
+// Created on: Mar 2025
 // This file contains the JS functions for index.html
+
+function myButtonClicked() {
+  // input
+  const radius = parseFloat(document.getElementById("radius").value)
+  //process
+  const volume = 3/4*Math.PI*radius**3
+  // output
+  document.getElementById("answer").innerHTML =
+    "Volume is: " + volume.toFixed(2) + "cm³"
+}
